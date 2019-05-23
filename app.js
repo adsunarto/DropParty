19ab7ac8d07a183c550242d3a55620359faa403c
Andrew Sunarto <adsunarto@Andrews-MacBook-Pro.local>
2019-05-23 16:00:23 -0500

    ...

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Connection URL
+// const url = 'mongodb://localhost:27017';
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Database Name
+// const dbName = 'DropParty';
+
+// Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+ 
+//   const db = client.db(dbName);
+ 
+//   client.close();
+// });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
